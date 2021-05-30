bb76cd4d3bdcbcc2766abd8da6a55ad627f5eec8
abhinavgupta <abhinav81003@gmail.com>
2021-05-30 12:25:32 +0530

    Final

--- public/app.js
+++ public/app.js
@@ -71,7 +71,7 @@
     // Signed in
     window.top.close();
     window.open("sign_in.html");  
-    var user = userCredential.user;
+    user = userCredential.user;
      })
     .catch((error) => {
     var errorCode = error.code;
@@ -83,17 +83,66 @@
      alert("Enter Username and Password")
    }
   }
-
+var user;
 
   function SignOut(){
     window.top.close();
     window.open("sign_in.html"); 
   }
 
+  //sidebar edit
   var canvas = document.getElementById("Canvas");
 if (canvas.getContext) 
 {
   var ctx = canvas.getContext('2d');
   ctx.fillStyle='rgb(109, 104, 246)';    // color of fill
   ctx.fillRect(10, 40, 140, 160); // create rectangle  
+}
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: '### FIREBASE API KEY ###',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+});
+
+//update 
+function update(){
+  var spo2=document.getElementById("spo2");
+  var fever=document.getElementById("fever");
+  var cough = document.getElementById("Cough");
+  var breath =document.getElementById("Breath");
+  var db = firebase.firestore();
+
+db.collection("logs").doc(user).set({
+  SPO2: spo2.value,
+  Fever: fever.value,
+  Cough: cough.value,
+  Breathlessness: breath.value,
+})
+.then((user) => {
+  console.log("Document written with ID: ", user);
+})
+.catch((error) => {
+  console.error("Error adding document: ", error);
+});
+add();
+}
+
+function add(){
+  var db = firebase.firestore();
+  var docRef = db.collection("logs").doc(user);
+docRef.get().then((doc) => {
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+      var element = document.getElementById("data");
+      element.innerHTML = doc.data();
+        
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
 }
